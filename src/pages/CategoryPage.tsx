cd311af8a7b35b82d59fc3a28533a0cb73298152
millan-figueroa <millan.fig@gmail.com>
2025-11-23 00:03:03 -0800

    Imports useParams in CategoryPage and RecipeDetailPage to access dynamic route params for category and recipe details

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,9 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function CategoryPage() {
+  const { categoryName } = useParams();
   // use categoryName to ftch recipes?
   return (
     <div>
-      <h1 className="text-xl font-semibold">Category</h1>
-      {/* recipes list */}
+      <h1 className="text-xl font-semibold">Category: {categoryName}</h1>
+      {/* recipe list goes here */}
     </div>
   );
 }
--- src/pages/RecipeDetailPage.tsx
+++ src/pages/RecipeDetailPage.tsx
@@ -1,9 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function RecipeDetailPage() {
+  const { recipeId } = useParams();
   // use reciapeId to fetch recipe details
   // faves context for add/remove favorite
   return (
     <div>
-      <h1 className="text-xl font-semibold">recipe detail</h1>
+      <h1 className="text-xl font-semibold">Recipe Detail: {recipeId}</h1>
       {/* recipe deets */}
     </div>
   );
